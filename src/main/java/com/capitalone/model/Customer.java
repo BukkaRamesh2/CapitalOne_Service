f4729667a03059b2a165fcd9fb4dcc387eaf1a67
BukkaRamesh <bukkaramesh.b@gmail.com>
2024-08-13 08:31:12 -0500

    customer model class

---
+++ src/main/java/com/capitalone/model/Customer.java
@@ -0,0 +1,138 @@
+/**
+ * 
+ */
+package com.capitalone.model;
+
+/**
+ * ramesh b
+ */
+public class Customer {
+	
+	// accessspecifier datatype name
+	private Long customerId;  // 12   "12"
+	private String name;
+	private String address;
+	private int phoneNumber;
+	private String email;
+	private boolean status;
+	private double balance;
+	
+	/*
+	 *   access specifiers
+	 *   
+	 *   private  -- the scope exists only in the class
+	 *   public  -- the scope exists entire applications
+	 *   protected -- the scope is to access only inside packages
+	 *   default -- is same as public
+	 *   
+	 *   
+	 *   data types 
+	 *     int -- stores integers
+	 *     float  -- decimal values -- up to 2 decimals
+	 *     double  -- decimal values with more memeory to store 
+	 *     string  -- combination of characters
+	 *     long  -- intergers with more memory 
+	 *     boolean  -- true/false  0/1
+	 *     character  -- single
+	 *  
+	 *   Wrapper classes can be used to type case
+	 *    INTEGER 
+	 *    Long -- type case 
+	 *    
+	 *     
+	 *   Constructors
+	 *   
+	 *     -- class name
+	 *     -- n numbner of constructors with smae class name
+	 *     
+	 * 
+	 * 
+	 */
+
+	/**
+	 * 
+	 */
+	public Customer() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Customer(Long customerId) {
+		// TODO Auto-generated constructor stub
+		this.customerId = customerId;
+	}
+	
+	public Customer(Long customerId, String name, String address, int phoneNumber, String email, boolean status,
+			double balance) {
+		super();
+		this.customerId = customerId;
+		this.name = name;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.status = status;
+		this.balance = balance;
+	}
+
+	public Long getCustomerId(){
+		return customerId;
+	}
+	
+    public void setCustomerId(Long customerId){
+		this.customerId = customerId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	
+    
+    
+    
+	
+
+}
---
+++ src/main/java/com/capitalone/util/Test.java
@@ -0,0 +1,43 @@
+package com.capitalone.util;
+
+import com.capitalone.model.Customer;
+
+public class Test {
+	
+	
+	
+	/*
+	 *   General info on captial one 
+	 *   
+	 *   
+	 *   
+	 *   Customers  -- ramesh
+	 *   Transactions -- mani
+	 *   Services  -- nikhil
+	 *   Accounts -- abhi
+	 *   Card -- prasanna 
+	 *   
+	 *   
+	 *   ****
+	 *   
+	 *   Model / pojo  -- objects associatyed to attributes and methods
+	 *   Controller   -- Receive the request from browser and pass to service to perform business logic
+	 *   Service  -- preocess all the business logic and send the final object to repository
+	 *   Repository  -- save the data to database and get the data based on customer queries
+	 *   Exceptions  -- global exceptions used t0 have a good definations for errors to understand by customers
+	 *   Util -- general classes 
+	 *   
+	 *   
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	
+	
+	public static void main(String[] args) {
+		Customer c = new Customer(123L);
+	
+	}
+
+}
